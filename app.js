541500a63bebba8dbbee304b750732cb89263067
Nachiket <nachiket.moghe@somaiya.edu>
2020-09-14 23:04:33 +0530

    Signup authentication done, login left.

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 var express= require('express');
-var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var passport = require('passport');
+var User = require("./models/user");
 var LocalStrategy = require('passport-local');
 var passportLocalMongoose = require('passport-local-mongoose');
-// var fs = require('fs');
-
-// var myCss = {
-//          style : fs.readFileSync('./views/style_load.css','utf8')
-// };
 
 
 mongoose.set('useNewUrlParser', true);
@@ -18,32 +13,36 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 mongoose.connect("mongodb://localhost:27017/load_adjustment_portal");
+
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.use(require("express-session")({
+    secret: "This internship is pretty cool",
+    resave: false,
+    saveUninitialized: false 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-app.set('view engine', 'ejs');
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-
-
+// ++++++++++
+// Routes   +
+// ++++++++++
 
 app.get("/", function(req,res){
-    // if(err)
-    // {res.re}
+
     res.render('home');
 });
 
-app.listen(process.env.PORT||3000, process.env.ip, function(){
-    console.log('Server Activated..');
-});
-
 app.get("/load_form", function(req, res){
-    // if(err){
-    //     res.render(err);
-    // }
-    // else{
-        res.render('load_adjust_form');
-        
-    // }
+    res.render('load_adjust_form');
 });
 
 app.get("/login", function(req,res){
@@ -54,3 +53,25 @@ app.get("/login", function(req,res){
 app.get("/signup", function(req,res){
     res.render('signup');
 });
+
+app.post("/signup", function(req,res){
+    req.body.username
+    req.body.firstname
+    req.body.lastname
+    req.body.email
+    req.body.password
+    User.register(new User({username: req.body.username, firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email
+    }), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("signup");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/load_form");
+        });
+    });
+});
+app.listen(process.env.PORT||3000, process.env.ip, function(){
+    console.log('Server Activated..');
+});
+
---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var passportLocalMongoose = require("passport-local-mongoose");
+var userSchema = new mongoose.Schema({
+    firstname : String,
+    lastname : String,
+    email : String,
+    username: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports= mongoose.model("User", userSchema);
